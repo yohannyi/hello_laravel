9b69c938b5431dec93b9540180b1ac802938a6dc
yohann <390657063@qq.com>
2021-05-25 07:43:09 +0000

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
